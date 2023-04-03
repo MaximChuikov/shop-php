1f4bb60c7f6130ec47c093be46c34dbb05a6f1bb
Maxim <machsim26@gmail.com>
2023-04-03 19:01:36 +0500

    registration login and logout

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\BasketIsNotEmpty;
+use App\Http\Middleware\CheckIsAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -54,6 +56,8 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'is_admin' => CheckIsAdmin::class,
+        'basket_not_empty' => BasketIsNotEmpty::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,42 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\BasketController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/basket', [BasketController::class, 'basket'])->name('basket');
-Route::get('/basket/place', [BasketController::class, 'basketPlace'])->name('basket-place');
-Route::post('/basket/add/{id}', [BasketController::class, 'basketAdd'])->name('basket-add');
-Route::post('/basket/remove/{id}', [BasketController::class, 'basketRemove'])->name('basket-remove');
-Route::post('/basket/place', [BasketController::class, 'basketConfirm'])->name('basket-confirm');
+Auth::routes([
+    'reset' => false,
+    'confirm' => false,
+    'verify' => false,
+]);
+Route::group([
+    'middleware' => 'auth',
+    'namespace' => 'Admin',
+], function () {
+    Route::group(['middleware' => 'is_admin'], function () {
+        Route::get('/orders', 'OrderController@index')->name('home');
+    });
+});
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('get-logout');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'basket'], function () {
+    Route::post('/add/{id}', [BasketController::class, 'basketAdd'])->name('basket-add');
+
+    Route::group([
+        'middleware' => 'basket_not_empty',
+    ], function () {
+        Route::get('/', [BasketController::class, 'basket'])->name('basket');
+        Route::get('/place', [BasketController::class, 'basketPlace'])->name('basket-place');
+        Route::post('/remove/{id}', [BasketController::class, 'basketRemove'])->name('basket-remove');
+        Route::post('/place', [BasketController::class, 'basketConfirm'])->name('basket-confirm');
+    });
+});
 
 Route::get('/', [MainController::class, 'index'])->name('index');
 Route::get('/categories', [MainController::class, 'categories'])->name('categories');
